e04217b64abc7a60c323604b1866af0aac04f1c8
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2021-12-19 10:38:30 +0200

    Use css grid instead of table in ui

--- src/app/ui.cljs
+++ src/app/ui.cljs
@@ -1,5 +1,6 @@
 (ns app.ui
   (:require
+   [clojure.string :as str]
    [reagent.core :as r]
    [app.game :as game]))
 
@@ -70,59 +71,67 @@
      :game.status/boom "🌚"
      "🌝")])
 
-(defn table
-  []
+(defn- cell
+  [component coords border]
+  (let [cell    (-> @state :game/game :game/board (get coords))
+        hidden? (#{:cell.state/hidden :cell.state/flagged}
+                 (:cell/state cell))]
+    [component
+     {:on-click        (fn [evt]
+                         (.preventDefault evt)
+                         (play! (:game/game @state) coords))
+      :on-context-menu (fn [evt]
+                         (.preventDefault evt)
+                         (toggle-flag! (:game/game @state) coords))
+      :style
+      {:display          "grid"
+       :align-items      "center"
+       :aspect-ratio     "1"
+       :cursor           (if hidden? "pointer" "default")
+       :box-shadow       (if hidden?
+                           #_"2px 2px #ffffff inset, -2px -2px grey inset"
+                           ".08em .08em #ffffff inset, -.08em -.08em grey inset"
+                           "initial")
+       :border           border
+       :background-color "#c0c0c0"
+       :font-weight      "bold"
+       :font-size        "200%"
+       :text-align       "center"
+       :color            (value-colors (:cell/value cell))}}
+
+     ;; Cell content
+     [:div
+      {:style
+       {:text-align "center"}}
+
+      (condp = (:cell/state cell)
+        :cell.state/hidden #_\u2003 ;; ZWSP
+        \u2003
+
+        :cell.state/flagged
+        [:span {:style {:font-size ".8em"}} "🚩"]
+
+        (if (= :cell.value/boom (:cell/value cell))
+          [:span {:style {:font-size ".8em"}} "🙀"]
+          (str (:cell/value cell))))]]))
+
+(defn grid []
   (let [width  (-> @state :game/level levels :game/width)
         height (-> @state :game/level levels :game/height)
         border (case (get-in @state [:game/game :game/status])
                  :game.status/boom "1px solid red"
                  :game.status/win  "1px solid green"
                  "1px solid gray")]
-    [:table
-     {:cellSpacing 0
-      :styl
-      {:border           border
-       :background-color "#808080"}}
-
-     (into
-      [:tbody]
-      (for [row (range height)]
-        (into
-         [:tr]
-         (for [col  (range width)
-               :let [coords [col row]
-                     cell    (-> @state :game/game :game/board (get coords))
-                     hidden? (#{:cell.state/hidden :cell.state/flagged}
-                              (:cell/state cell))]]
-           ;; Cell
-           [:td
-            {:on-click        (fn [evt]
-                                (.preventDefault evt)
-                                (play! (:game/game @state) coords))
-             :on-context-menu (fn [evt]
-                                (.preventDefault evt)
-                                (toggle-flag! (:game/game @state) coords))
-             :style
-             {:width            "1em"
-              :height           "1em"
-              :cursor           (if hidden? "pointer" "default")
-              :box-shadow       (if hidden?
-                                  "2px 2px #ffffff inset, -2px -2px grey inset"
-                                  "initial")
-              :border           border
-              :background-color "#c0c0c0"
-              :font-weight      "bold"
-              :font-size        "2em"
-              :text-align       "center"
-              :color            (value-colors (:cell/value cell))}}
-
-            ;; Cell content
-            (condp = (:cell/state cell)
-              :cell.state/hidden  \u2003 ;; ZWSP
-              :cell.state/flagged [:span {:style {:font-size "0.8em"}} "🚩"]
-              (if (= :cell.value/boom (:cell/value cell))
-                [:span {:style {:font-size "0.8em"}} "🙀"]
-                (str (:cell/value cell))))]))))]))
+    (into
+     [:div
+      {:style
+       {:display               "grid"
+        :max-width             (str (* 2.8 width) "em")
+        :grid-template-columns (->> "1fr" (repeat width) (str/join " "))}}]
+     (for [col  (range width)
+           row  (range height)
+           :let [coords [col row]]]
+       [cell :div coords border]))))
 
 (defn game
   []
@@ -130,6 +139,7 @@
    [:div {:style {:display "flex" :align-items "center"}}
     [level-selector]
     [reset-button]]
-   [table]])
+   #_[table]
+   [grid]])
 
 (-> @state :game/game :game/board (get [0 0]))
